561d65f8ab777ac1cc728bf38278d6d65e80f6c9
Andrew <baumanab@gmail.com>
2017-11-20 12:24:28 -0800

    optimize SetAssociations() by passing arguments by reference

--- src/particle_filter.h
+++ src/particle_filter.h
@@ -102,7 +102,9 @@ public:
 	 * Set a particles list of associations, along with the associations calculated world x,y coordinates
 	 * This can be a very useful debugging tool to make sure transformations are correct and assocations correctly connected
 	 */
-	Particle SetAssociations(Particle particle, std::vector<int> associations, std::vector<double> sense_x, std::vector<double> sense_y);
+	Particle SetAssociations(Particle& particle, const std::vector<int>& associations,
+		                     const std::vector<double>& sense_x, const std::vector<double>& sense_y);
+
 	
 	std::string getAssociations(Particle best);
 	std::string getSenseX(Particle best);
